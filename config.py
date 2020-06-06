ec52abd3f916d57719bafeadc721c31331173e8a
Laica Lunasys <laica.lunasys@gmail.com>
2020-06-06 09:05:13 +0100

    Fix default IR GPIO port

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 import os
 http_port = os.getenv("PIGENT_HTTP_PORT", default=8081)
-ir_gpio = os.getenv("PIGENT_IR_GPIO", default=17)
+ir_gpio = os.getenv("PIGENT_IR_GPIO", default=4)
 bme280_address = os.getenv("BME280_ADDRESS", default=0x76)
