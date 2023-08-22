72249aab1694a579325823dfdbfdb7e0155275a1
Laica Lunasys <laica.lunasys@gmail.com>
2023-08-22 13:54:28 +0900

    Fix GPIO definition

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 http_port = os.getenv("PIGENT_HTTP_PORT", default=8081)
-ir_gpio = os.getenv("PIGENT_IR_GPIO", default=4)
+ir_gpio = int(os.getenv("PIGENT_IR_GPIO", default=4))
 
 # find from : i2cdetect -y 1
 bme280_address = int(os.getenv("BME280_ADDRESS", default="0x76"), 0)
